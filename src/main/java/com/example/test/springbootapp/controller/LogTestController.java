6e01f67249f56ca1031a9c95d4d3e5e437b9426f
Harry Pua <32326076+harrypua@users.noreply.github.com>
2019-09-18 11:38:24 +0800

    clean up comments

--- src/main/java/com/example/test/springbootapp/controller/LogTestController.java
+++ src/main/java/com/example/test/springbootapp/controller/LogTestController.java
@@ -29,23 +29,11 @@ public class LogTestController {
 		String txID = UUID.randomUUID().toString();
 		ThreadContext.put("txID", txID);
 
-		// sample
-		// {"@timestamp":"2019-09-18T10:25:07.662+08:00","appName":"test-log-app","level":"INFO","class":"com.example.test.springbootapp.SpringbootAppApplication","method":"testLogging","file":"SpringbootAppApplication.java","line":60,"thread":"main","txID":"cbd75e44-3ccd-41a0-a19c-e537476befe5","value1":1,"msg":"this
-		// is a test log message"}
-
 		log.info("this is a test log message", v("value1", 1));
-
-		// sample
-		// {"@timestamp":"2019-09-18T10:25:07.693+08:00","appName":"test-log-app","level":"INFO","class":"com.example.test.springbootapp.SpringbootAppApplication","method":"testLogging","file":"SpringbootAppApplication.java","line":63,"thread":"main","txID":"cbd75e44-3ccd-41a0-a19c-e537476befe5","value2":2,"msg":"this
-		// is a 2nd test log message"}
 
 		log.info("this is a 2nd test log message", v("value2", 2));
 
 		TestObj testObj = new TestObj(11111, "content of test obj");
-
-		// sample
-		// {"@timestamp":"2019-09-18T10:26:20.142+08:00","appName":"test-log-app","level":"INFO","class":"com.example.test.springbootapp.SpringbootAppApplication","method":"testLogging","file":"SpringbootAppApplication.java","line":71,"thread":"main","txID":"d537dccd-0534-45f8-810d-960768f85585","obj1":{"id":11111,"content":"content
-		// of test obj"},"msg":"testing obj in context"}
 
 		log.info("testing obj in context", v("obj1", testObj));
 
